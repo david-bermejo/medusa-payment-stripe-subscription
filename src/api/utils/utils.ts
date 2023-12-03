823e7157f37094a06d46f87176a6a53d19840d1a
David Bermejo <davidbermejosanta@gmail.com>
2023-12-03 19:44:28 +0100

    Renamed disconnect -> delete

--- src/api/utils/utils.ts
+++ src/api/utils/utils.ts
@@ -158,7 +158,7 @@ async function onSubscriptionDeleted({
 }) {
     const subscriptionService = container.resolve("subscriptionService")
 
-    await subscriptionService.disconnect(subscription.id)
+    await subscriptionService.delete(subscription.id)
 }
 
 async function onInvoicePaymentFailed({
